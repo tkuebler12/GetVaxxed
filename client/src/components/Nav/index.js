4cc28069d868c2aca627da9963492ca8a7abc362
shuting <zst9394@outlook.com>
2021-05-12 20:15:48 -0400

    add react Home & Nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,23 +1,32 @@
-import Navbar from "react-bootstrap/Navbar";
+import React from "react";
 
-<Navbar bg="light" expand="lg">
-	<Navbar.Brand href="#home">Get-VAXXED</Navbar.Brand>
-	<Navbar.Toggle aria-controls="basic-navbar-nav" />
-	<Navbar.Collapse id="basic-navbar-nav">
-		<Nav className="mr-auto">
-			<Nav.Link href="#home">Home</Nav.Link>
-			<Nav.Link href="#link">Link</Nav.Link>
-			<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-				<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-				<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-				<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-				<NavDropdown.Divider />
-				<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-			</NavDropdown>
-		</Nav>
-		<Form inline>
-			<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-			<Button variant="outline-success">Search</Button>
-		</Form>
-	</Navbar.Collapse>
-</Navbar>;
+function Nav() {
+  return (
+    <header>
+        <nav className="navbar-expand-lg navbar navbar-dark bg-primary">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/"> Get vaxxed!!!!!</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#contact">Contact</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#portfolio">Portfolio</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#aboutme">About</a>
+                  </li>
+                
+                </ul>
+              </div>
+            </div>
+        </nav>
+        </header>
+  );
+}
+
+export default Nav;
