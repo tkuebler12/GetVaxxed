13ddfee766effaf930770e5bcbf23ee312c1d2ae
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-06-01 20:16:41 -0400

    deleted seed file

--- seeders/seeds.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require("mongoose");
-let db = require("../models/location");
-
-mongoose.connect("mongodb://localhost/covid", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
